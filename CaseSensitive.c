9f5e3ecb30847d204045551ae3a6c5e9a13883af
Qhroal12 <kfc030306@gmail.com>
2026-03-12 23:43:36 +0900

    Enhance CaseSensitive.c with comments

--- CaseSensitive.c
+++ CaseSensitive.c
@@ -9,16 +9,17 @@ int main(void) {
 	gets(str);
 
 	for (int i = 0; str[i] != '\0'; ++i)
-		length += 1;
+		length += 1;							//문자열 길이 계산
 
 	for (int i = 0; i < length; ++i) {
-		if (str[i] > 96 && str[i] < 123)		//소문자에서 대문자
+		if (str[i] > 96 && str[i] < 123)		//알파벳 소문자 범위의 변수 대문자로 변환
 			str[i] = str[i] - 32;
-		else if (str[i] > 64 && str[i] < 90)	//대문자에서 소문자
+		else if (str[i] > 64 && str[i] < 90)	//알파벳 대문자 범위의 변수 소문자로 변환
 			str[i] = str[i] + 32;
 	}
 
 	printf("%s", str);
 
 	return 0;
+
 }
